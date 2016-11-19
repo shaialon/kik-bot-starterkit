88cae61d5d35e5acca19a7470cae7974cb327742
Shai Alon <shaialon84@gmail.com>
2016-11-19 12:48:35 +0200

    cool

--- config/index.js
+++ config/index.js
@@ -2,9 +2,15 @@ module.exports = {
   local: {
     port: 8083,
     kik: {
-      apiKey: '81cefe4a-9086-482a-a356-218ed06b7636', // [Non functional example] 0a275f34-31da-4ds1-b3c9-d4576f38bac8
-      botUsername: 'shaibot', // [Non functional example] startkitbot
+      //apiKey: '81cefe4a-9086-482a-a356-218ed06b7636', // [Non functional example] 0a275f34-31da-4ds1-b3c9-d4576f38bac8
+      //botUsername: 'shaibot', // [Non functional example] startkitbot
+      //baseUrl: 'https://shaikik.ngrok.io' // [Non functional example] http://4b7e3c1e.ngrok.io
+
+      apiKey: '2d718b92-52a6-4df2-9c32-e4540ac1fc58', // [Non functional example] 0a275f34-31da-4ds1-b3c9-d4576f38bac8
+      botUsername: 'rollagame', // [Non functional example] startkitbot
       baseUrl: 'https://shaikik.ngrok.io' // [Non functional example] http://4b7e3c1e.ngrok.io
+
+
     }
   }
 }['local']
